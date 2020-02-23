8c153833ac13ef7c2f97d5c780cb84a67a7d2386
Natalya Drevnik <natalyadrevnik@mail.ru>
2020-02-23 15:16:22 +0300

    Update tests

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,16 +1,14 @@
 package test;
 
 import lombok.val;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import data.DataHelper;
 import page.DashboardPage;
 import page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.sql.SQLException;
-
 
 public class LoginTest {
     @AfterEach
@@ -20,11 +18,16 @@ public class LoginTest {
         dataHelperPage.cleanDataBase();
     }
 
+    @BeforeEach
+    void login() {
+        val loginPage = new LoginPage();
+        loginPage.openLoginPage();
+    }
+
+
     @Test
     @DisplayName("Should login with code from sms")
     void shouldLoginWithSmsCode() throws SQLException {
-        val loginPage = new LoginPage();
-        loginPage.openLoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verificationPage();
@@ -36,8 +39,6 @@ public class LoginTest {
     @Test
     @DisplayName("Should be error notification if login with wrong password")
     void loginWithWrongPassword() {
-        val loginPage = new LoginPage();
-        loginPage.openLoginPage();
         val authInfo = DataHelper.getAuthInfoWithWrongPassword();
         loginPage.validLogin(authInfo);
         loginPage.errorNotificationCreate();
@@ -46,8 +47,6 @@ public class LoginTest {
     @Test
     @DisplayName("Should login with random user and status active")
     void shouldLoginWithRandomUser() throws SQLException {
-        val loginPage = new LoginPage();
-        loginPage.openLoginPage();
         val authInfo = DataHelper.getRandomAuthInfo("active");
         val verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verificationPage();
